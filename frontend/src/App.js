7b94df449723b57b1036760f99789b0b34b644ef
Vishal-Upadhyay2403 <vishalu299@gmail.com>
2025-07-02 14:00:40 +0530

    spinner in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
 import Welcome from './components/Welcome';
+import Spinner  from './components/Spinner';
 import { Container,Row,Col, Image } from 'react-bootstrap';
 
 const API_URL = process.env.API_URL || 'http://192.168.0.105:5050';
@@ -14,11 +15,13 @@ const API_URL = process.env.API_URL || 'http://192.168.0.105:5050';
 const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getSaveImages = async () => {
     try {
       const res = await axios.get(`${API_URL}/images`);
       setImages(res.data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -97,19 +100,30 @@ const handleSaveImage = async (id) => {
   return (
     <div className="App">
       <Header title="Images Gallery"/>
-      <Search word={word} setWord = {setWord} handleSubmit={handleSerachSubmit}/>
-      <Container className='mt-4'>
-        {images.length ? (
-          <Row xs={1} md={2} lg={3}>
-            {images.map((image, i) => (
-            <Col key={i} className='pd-3'>
-            <ImageCard  image={image} deleteImage= {handleDeleteImage} 
-            saveImage = {handleSaveImage} />
-          </Col>
-      ))}
-        </Row>) : ( <Welcome/> 
-      ) }
-      </Container>
+      {loading ? ( <Spinner/> ) : 
+      (
+         <>
+            <Search word={word} setWord = {setWord} handleSubmit={handleSerachSubmit}/>
+            <Container className='mt-4'>
+            {images.length ? (
+            <Row xs={1} md={2} lg={3}>
+              {
+                images.map((image, i) => (
+                  <Col key={i} className='pd-3'>
+                  <ImageCard  image={image} deleteImage= {handleDeleteImage} 
+                  saveImage = {handleSaveImage} />
+                  </Col>
+                ))
+              }
+              </Row>) :
+              ( 
+                <Welcome/> 
+              ) 
+            }
+            </Container>
+          </>
+        )
+      }
     </div>
   );
 }
